d661fdf692ccc98222ab94c0743f11891512292c
mayara_Abreu <mayara.arj@gmail.com>
2022-04-16 15:20:51 -0300

    Criação do exercicio

---
+++ calculaValorDevido.js
@@ -0,0 +1,5 @@
+//Depois de nossa consultoria, a lavanderia DigitalLaundry percebeu que poderia deixar a sua cobrança mais sofisticada e justa. Ela decidiu cobrar R$10,00 fixo, a título de taxa de serviço (independente da quantidade de roupa), mais R$ 3,00 por quilo de roupa suja. Reescreva a função calculaValorDevido
+
+function calculaValorDevido(pesoDeRoupaSuja){
+    return 10 + pesoDeRoupaSuja*3
+}
